e53d7589d503658c4b428cabab42efffeb98b545
Andres A. <91929532+404nicknotfound@users.noreply.github.com>
2026-05-23 16:48:00 -0500

    Refactor FastAPI application with MongoDB connection

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ app.add_middleware(
 )
 
 #os.environ para despliegue. Descomente cuando ya probó todo local.
-#client = MongoClient(os.environ["MONGO_URI"])
+client = MongoClient(os.environ["MONGO_URI"])
 
 # TODO: conectarse al cluster Admonsis  
 # client = MongoClient("mongodb://<usuario>:<contraseña>@157.253.236.88:8087")
-client = MongoClient("mongodb://ISIS2304I03202610:zRrlxLM4A0Vl@157.253.236.88:8087")
+#client = MongoClient("mongodb://ISIS2304I03202610:zRrlxLM4A0Vl@157.253.236.88:8087")
 # TODO: conectarse a la base de datos Admonsis  
 # = client["ISIS2304"]
 db = client["ISIS2304I03202610"]
